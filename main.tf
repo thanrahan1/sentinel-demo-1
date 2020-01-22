953fa0f6b1e58ffde3ecf89396f7d18fbe5e3233
Thomas Hanrahan <thanrahan1@comcast.net>
2020-01-22 08:45:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,8 +33,8 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  count = 2
   tags = {
-    Name = "Spider-Girl"
-    count = 2
-  }
+        Name = "Spider-Girl-${count.index}"
+   }
 }
